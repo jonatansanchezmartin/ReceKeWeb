d344aff9996e473305dad9ac9d9cd71b058fe789
Jonatan Sanchez Martin <sanchez.martin.jonatan@gmail.com>
2024-07-09 18:38:02 +0200

    deployed3

--- api/index.php
+++ api/index.php
@@ -372,6 +372,42 @@ if (empty($_POST['email'])) {
       }
   }
 
+
+  // $name = $_POST["nombre"];
+// $apellidos = $_POST["apellidos"];
+// $email = $_POST["email"];
+// $mensaje = $_POST["mensaje"];
+
+// require "vendor/autoload.php";
+
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\SMTP;
+
+// $mail = new PHPMailer(true);
+
+// $mail->isSMTP();
+// $mail->STMPAuth = true;
+
+// $mail->Host = "";
+// $mail->SMTPSecure = PHPMailer::ENCRIPTYON_STARTTLS;
+// $mail->Port = ;
+
+// $mail->Username = "";
+// $mail->password = "";
+
+
+// $mail->setFrom($email, $name, $apellidos);
+// $mail->addAddress("jonatansm@jonatansanchez.org", "Jonatan");
+
+// $mail->Subject = $subject;
+// $mail->Body = $message;
+
+
+
+// $mail->send();
+
+// echo "Email enviado"
+
 ?>
 
     
